716ec6a63f8f959cab7b96eed3427102a9c1cb2c
Marcin P <marcin.pil@gmail.com>
2015-09-17 21:23:13 +0200

    Initial project layout generated from Spring Initializr
    Spring Initializr is an online tool: https://start.spring.io/

---
+++ src/main/java/com/github/yu55/gog/GogApplication.java
@@ -0,0 +1,12 @@
+package com.github.yu55.gog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GogApplication.class, args);
+    }
+}
