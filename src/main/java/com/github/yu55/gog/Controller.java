e4024c8d90870bf2def1986a08384668f509239f
Marcin P <marcin.pil@gmail.com>
2015-09-22 23:33:03 +0200

    Executing command. Very nasty code - prototyping.

--- src/main/java/com/github/yu55/gog/Controller.java
+++ src/main/java/com/github/yu55/gog/Controller.java
@@ -1,11 +1,17 @@
 package com.github.yu55.gog;
 
+import org.apache.commons.exec.*;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.LinkedList;
 import java.util.List;
 
 @RestController()
@@ -13,10 +19,40 @@ public class Controller {
 
     @RequestMapping(value = "/grep", method = RequestMethod.POST)
     public List<String> grep(@RequestBody String wanted) {
-        System.out.println("wanted=" + wanted);
-        return Arrays.asList(new String[]{
-                "src/main/java/com/github/yu55/gog/Application.java:    public static void main(String[] args) {",
-                "src/main/java/com/github/yu55/gog/Controller.java:    List<String> grep() {",
-                "src/main/java/com/github/yu55/gog/Controller.java:        return Arrays.asList(new String[] {\"\", \"\", \"\"});"});
+        return getStrings(wanted);
+    }
+
+    private List<String> getStrings(String wanted) {
+        CommandLine commandLine = CommandLine.parse("git grep -n " + wanted);
+        DefaultExecutor executor = new DefaultExecutor();
+        Path dir = new File("\\repositories").toPath();
+        final List<String> allLines = new LinkedList<>();
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+            for (Path entry: stream) {
+                if(entry.toFile().isDirectory()) {
+                    final List<String> lines = new LinkedList<>();
+                    executor.setWorkingDirectory(entry.toFile());
+
+                    executor.setStreamHandler(new PumpStreamHandler(new LogOutputStream() {
+                        @Override
+                        protected void processLine(String line, int logLevel) {
+                            lines.add(line);
+                        }
+                    }));
+
+                    try {
+                        executor.execute(commandLine);
+                        allLines.addAll(lines);
+                    } catch (ExecuteException e) {
+                        System.out.println("Uuups: " + e.getMessage());
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println("catch ex=" + ex.getMessage());
+        }
+
+        System.out.println("return allLines=" + allLines);
+        return allLines;
     }
 }
