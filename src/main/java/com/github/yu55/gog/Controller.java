bc8d27d67920424b48288fdf69ec9f1d86bfcbf8
Marcin P <marcin.pil@gmail.com>
2015-09-22 21:30:18 +0200

    POST wanted text on form submit

--- src/main/java/com/github/yu55/gog/Controller.java
+++ src/main/java/com/github/yu55/gog/Controller.java
@@ -1,6 +1,8 @@
 package com.github.yu55.gog;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -9,8 +11,9 @@ import java.util.List;
 @RestController()
 public class Controller {
 
-    @RequestMapping("/grep")
-    public List<String> grep() {
+    @RequestMapping(value = "/grep", method = RequestMethod.POST)
+    public List<String> grep(@RequestBody String wanted) {
+        System.out.println("wanted=" + wanted);
         return Arrays.asList(new String[]{
                 "src/main/java/com/github/yu55/gog/Application.java:    public static void main(String[] args) {",
                 "src/main/java/com/github/yu55/gog/Controller.java:    List<String> grep() {",
