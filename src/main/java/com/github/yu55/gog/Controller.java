9910a588a57df28c9e25809bbd3027e1443095ee
Marcin P <marcin.pil@gmail.com>
2015-09-23 22:04:17 +0200

    'Select multiple' with repositories introduced.

--- src/main/java/com/github/yu55/gog/Controller.java
+++ src/main/java/com/github/yu55/gog/Controller.java
@@ -7,15 +7,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController()
 public class Controller {
 
+    private Repositories repositories;
+
     private GrepService grepService;
 
     @Autowired
-    public Controller(GrepService grepService) {
+    public Controller(GrepService grepService, Repositories repositories) {
         this.grepService = grepService;
+        this.repositories = repositories;
+    }
+
+    @RequestMapping(value = "/repositories")
+    public List<String> repositories() {
+        return repositories.getDirectories().stream().map(d -> d.getName()).collect(Collectors.toList());
     }
 
     @RequestMapping(value = "/grep", method = RequestMethod.POST)
