233a354b480c1ba22a502701536136329cb17e22
Marcin P <marcin.pil@gmail.com>
2015-09-23 21:27:57 +0200

    GrepService component introduced.

--- src/main/java/com/github/yu55/gog/Controller.java
+++ src/main/java/com/github/yu55/gog/Controller.java
@@ -1,62 +1,26 @@
 package com.github.yu55.gog;
 
-import org.apache.commons.exec.CommandLine;
-import org.apache.commons.exec.DefaultExecutor;
-import org.apache.commons.exec.LogOutputStream;
-import org.apache.commons.exec.PumpStreamHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.LinkedList;
 import java.util.List;
 
 @RestController()
 public class Controller {
 
-    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
-
-    private Repositories repositories;
+    private GrepService grepService;
 
     @Autowired
-    public Controller(Repositories repositories) {
-        this.repositories = repositories;
+    public Controller(GrepService grepService) {
+        this.grepService = grepService;
     }
 
     @RequestMapping(value = "/grep", method = RequestMethod.POST)
     public List<String> grep(@RequestBody String wanted) {
-        return getStrings(wanted);
-    }
-
-    private List<String> getStrings(String wanted) {
-        CommandLine commandLine = CommandLine.parse("git grep -n " + wanted);
-        DefaultExecutor executor = new DefaultExecutor();
-        List<File> directories = repositories.getDirectories();
-        List<String> allFindings = new LinkedList<>();
-        List<String> findings = new LinkedList<>();
-        executor.setStreamHandler(new PumpStreamHandler(new LogOutputStream() {
-            @Override
-            protected void processLine(String line, int logLevel) {
-                findings.add(line);
-            }
-        }));
-        for (File directory : directories) {
-            executor.setWorkingDirectory(directory);
-            try {
-                executor.execute(commandLine);
-                allFindings.addAll(findings);
-            } catch (IOException e) {
-                logger.warn("Problem while searching '{}' repository: {}", directory.getAbsolutePath(), e.getMessage());
-            }
-            findings.clear();
-        }
-        return allFindings;
+        return grepService.grep(wanted);
     }
 
 }
---
+++ src/main/java/com/github/yu55/gog/GrepService.java
@@ -0,0 +1,70 @@
+package com.github.yu55.gog;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.LogOutputStream;
+import org.apache.commons.exec.PumpStreamHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+@Component
+public class GrepService {
+
+    private static final Logger logger = LoggerFactory.getLogger(GrepService.class);
+
+    private static final String COMMAND = "git grep -n ";
+
+    private Repositories repositories;
+
+    @Autowired
+    public GrepService(Repositories repositories) {
+        this.repositories = repositories;
+    }
+
+    public List<String> grep(String wanted) {
+        List<String> allFindings = new LinkedList<>();
+        List<File> directories = repositories.getDirectories();
+
+        CommandLine commandLine = CommandLine.parse(COMMAND + wanted);
+        DefaultExecutor executor = new DefaultExecutor();
+        ExecutorStreamHandler executorStreamHandler = new ExecutorStreamHandler();
+        executor.setStreamHandler(new PumpStreamHandler(executorStreamHandler));
+        for (File directory : directories) {
+            executor.setWorkingDirectory(directory);
+            try {
+                executor.execute(commandLine);
+                allFindings.addAll(executorStreamHandler.getFindings());
+            } catch (IOException e) {
+                logger.warn("Problem while searching '{}' repository: {}", directory.getAbsolutePath(), e.getMessage());
+            }
+            executorStreamHandler.clearFindings();
+        }
+
+        return allFindings;
+    }
+
+    private class ExecutorStreamHandler extends LogOutputStream {
+
+        private List<String> findings = new LinkedList<>();
+
+        @Override
+        protected void processLine(String line, int logLevel) {
+            findings.add(line);
+        }
+
+        public List<String> getFindings() {
+            return findings;
+        }
+
+        public void clearFindings() {
+            findings.clear();
+        }
+    }
+}
