70d491a8de9189103c71742ff1a861541faaa4a1
Marcin P <marcin.pil@gmail.com>
2015-09-28 22:05:41 +0200

    Support for multiple repositories.

--- src/main/java/com/github/yu55/gog/Repositories.java
+++ src/main/java/com/github/yu55/gog/Repositories.java
@@ -1,31 +1,32 @@
 package com.github.yu55.gog;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.LinkedList;
-import java.util.List;
-
 @Component
 public class Repositories {
 
     private static final Logger logger = LoggerFactory.getLogger(Repositories.class);
 
-    private final String pathToRepositories;
+    private final List<String> pathsToRepositories;
 
     private final List<File> directories;
 
     @Autowired
-    Repositories(@Value("${repositories.path}") String pathToRepositories) {
-        this.pathToRepositories = pathToRepositories;
+    Repositories(@Value("${repositories.paths}") String[] pathsToRepositories) {
+        this.pathsToRepositories = Arrays.asList(pathsToRepositories);
         directories = new LinkedList<>();
     }
 
@@ -37,14 +38,16 @@ public class Repositories {
     }
 
     private void initDirectories() {
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(new File(pathToRepositories).toPath())) {
-            for (Path entry : stream) {
-                if (entry.toFile().isDirectory()) {
-                    directories.add(entry.toFile());
+        for (String ptr : pathsToRepositories) {
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(new File(ptr).toPath())) {
+                for (Path entry : stream) {
+                    if (entry.toFile().isDirectory()) {
+                        directories.add(entry.toFile());
+                    }
                 }
+            } catch (IOException ex) {
+                logger.error("Cannot obtain repositories directories", ex);
             }
-        } catch (IOException ex) {
-            logger.error("Cannot obtain repositories directories", ex);
         }
     }
 }
