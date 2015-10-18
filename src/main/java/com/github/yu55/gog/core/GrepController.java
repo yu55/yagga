7ee2326c864b0d0de4f1ce4d5676f018b65a4712
Marcin P <marcin.pil@gmail.com>
2015-10-18 15:37:55 +0200

    Working: dynamic rendering of annotated code.

--- src/main/java/com/github/yu55/gog/core/GrepController.java
+++ src/main/java/com/github/yu55/gog/core/GrepController.java
@@ -2,6 +2,7 @@ package com.github.yu55.gog.core;
 
 import static java.util.stream.Collectors.toList;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,24 +38,52 @@ public class GrepController {
     }
 
     @RequestMapping(value = "/blame", method = RequestMethod.POST)
-    public String blame() {//@RequestBody BlameRequest blameRequest) {
-//        return "package com.github.yu55.gog.core.model;\n" +
-//                "\n" +
-//                "public class BlameRequest {\n" +
-//                "\n" +
-//                "    private String file;\n" +
-//                "\n" +
-//                "    private String repository;\n" +
-//                "\n" +
-//                "    public String getFile() {\n" +
-//                "        return file;\n" +
-//                "    }\n" +
-//                "\n" +
-//                "    public String getRepository() {\n" +
-//                "        return repository;\n" +
-//                "    }\n" +
-//                "}";
-        return "public class BlameRequest {}";
+    public List<String> blame() {//@RequestBody BlameRequest blameRequest) {
+        return Arrays.asList("bfc83e2a        (Lukasz Sawicki 2015-10-14 21:32:16 +0200       1)\n" +
+                        "80dfaad5        (  Marcin P     2015-09-26 23:36:13 +0200       2)\n" +
+                        "80dfaad5        (  Marcin P     2015-09-26 23:36:13 +0200       3)\n" +
+                        "80dfaad5        (  Marcin P     2015-09-26 23:36:13 +0200       4)\n" +
+                        "80dfaad5        (  Marcin P     2015-09-26 23:36:13 +0200       5)\n" +
+                        "bfc83e2a        (Lukasz Sawicki 2015-10-14 21:32:16 +0200       6)\n" +
+                        "80dfaad5        (  Marcin P     2015-09-26 23:36:13 +0200       7)\n" +
+                        "bfc83e2a        (Lukasz Sawicki 2015-10-14 21:32:16 +0200       8)\n" +
+                        "80dfaad5        (  Marcin P     2015-09-26 23:36:13 +0200       9)\n" +
+                        "80dfaad5        (  Marcin P     2015-09-26 23:36:13 +0200       10)\n" +
+                        "80dfaad5        (  Marcin P     2015-09-26 23:36:13 +0200       11)\n" +
+                        "80dfaad5        (  Marcin P     2015-09-26 23:36:13 +0200       12)\n" +
+                        "80dfaad5        (  Marcin P     2015-09-26 23:36:13 +0200       13)\n" +
+                        "80dfaad5        (  Marcin P     2015-09-26 23:36:13 +0200       14)\n" +
+                        "bfc83e2a        (Lukasz Sawicki 2015-10-14 21:32:16 +0200       15)\n" +
+                        "bfc83e2a        (Lukasz Sawicki 2015-10-14 21:32:16 +0200       16)\n" +
+                        "bfc83e2a        (Lukasz Sawicki 2015-10-14 21:32:16 +0200       17)\n" +
+                        "bfc83e2a        (Lukasz Sawicki 2015-10-14 21:32:16 +0200       18)\n" +
+                        "80dfaad5        (  Marcin P     2015-09-26 23:36:13 +0200       19)\n" +
+                        "80dfaad5        (  Marcin P     2015-09-26 23:36:13 +0200       20)\n" +
+                        "80dfaad5        (  Marcin P     2015-09-26 23:36:13 +0200       21)\n" +
+                        "80dfaad5        (  Marcin P     2015-09-26 23:36:13 +0200       22)",
+                "package com.github.yu55.gog.core.model;\n" +
+                "\n" +
+                "import java.util.List;\n" +
+                "\n" +
+                "public class GrepRequest {\n" +
+                "\n" +
+                "    private String wanted;\n" +
+                "\n" +
+                "    private List<String> repositories;\n" +
+                "\n" +
+                "    public String getWanted() {\n" +
+                "        return wanted;\n" +
+                "    }\n" +
+                "\n" +
+                "    public boolean hasRepository(String name) {\n" +
+                "        return repositories.contains(name);\n" +
+                "    }\n" +
+                "\n" +
+                "    public List<String> getRepositories() {\n" +
+                "        return repositories;\n" +
+                "    }\n" +
+                "}");
+//        return "public class BlameRequest {}";
     }
 
 }
