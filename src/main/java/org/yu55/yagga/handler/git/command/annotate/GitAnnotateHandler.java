2d0a706c6fce5ec3d9327b9cbf128e786827f14f
Lukasz Sawicki <sawickil@gmail.com>
2015-11-07 00:04:19 +0100

    Draft of command execution error handling.

--- src/main/java/org/yu55/yagga/handler/git/command/annotate/GitAnnotateHandler.java
+++ src/main/java/org/yu55/yagga/handler/git/command/annotate/GitAnnotateHandler.java
@@ -1,17 +1,16 @@
 package org.yu55.yagga.handler.git.command.annotate;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.yu55.yagga.core.annotate.model.AnnotateRequest;
+import org.yu55.yagga.core.annotate.model.AnnotateResponse;
 import org.yu55.yagga.handler.git.GitRepositories;
 import org.yu55.yagga.handler.git.GitRepository;
 import org.yu55.yagga.handler.git.command.common.GitCommandExecutor;
+import org.yu55.yagga.handler.git.command.common.GitCommandOutput;
 import org.yu55.yagga.handler.git.command.common.GitCommandOutputLine;
-
-import org.yu55.yagga.core.annotate.model.AnnotateResponse;
 
 @Component
 public class GitAnnotateHandler {
@@ -36,18 +35,23 @@ public class GitAnnotateHandler {
 
     private AnnotateResponse executeAndConvertToResponse(GitRepository gitRepository,
                                                          GitCommandExecutor gitCommandExecutor) {
-        List<String> annotatedOutputLines = gitCommandExecutor.execute(gitRepository.getDirectory()).getOutputLines()
-                .stream()
-                .map(GitCommandOutputLine::getLine)
-                .collect(Collectors.toList());
+        GitCommandOutput gitCommandOutput = gitCommandExecutor.execute(
+                gitRepository.getDirectory());
+        List<GitCommandOutputLine> outputLines = gitCommandOutput.getOutputLines();
 
         StringBuilder annotationsStringBuilder = new StringBuilder();
         StringBuilder fileContentStringBuilder = new StringBuilder();
 
-        for (String annotatedLine: annotatedOutputLines) {
-            int splitIndex = annotatedLine.indexOf(")");
-            annotationsStringBuilder.append(annotatedLine.substring(0, splitIndex + 1)).append("\n");
-            fileContentStringBuilder.append(annotatedLine.substring(splitIndex + 1)).append("\n");
+        // TODO: this should be done smarter
+        for (GitCommandOutputLine outputLine: outputLines) {
+            String annotatedLine = outputLine.getLine();
+            if (gitCommandOutput.getExitValue() == 0) {
+                int splitIndex = annotatedLine.indexOf(")");
+                annotationsStringBuilder.append(annotatedLine.substring(0, splitIndex + 1)).append("\n");
+                fileContentStringBuilder.append(annotatedLine.substring(splitIndex + 1)).append("\n");
+            } else {
+                annotationsStringBuilder.append(annotatedLine).append("\n");
+            }
         }
 
         return new AnnotateResponse(annotationsStringBuilder.toString(), fileContentStringBuilder.toString());
