11715066ee8cc81be2fc7142f836cf137974def1
Adrian Woźniak <wozniak.adrian@gmail.com>
2015-12-12 22:00:21 +0100

    Allow to disable repositories update scheduler + properties refactoring

---
+++ src/main/java/org/yu55/yagga/handler/generic/RepositoriesUpdateScheduler.java
@@ -0,0 +1,39 @@
+package org.yu55.yagga.handler.generic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.yu55.yagga.handler.api.DvcsRepository;
+
+@Component
+@ConditionalOnProperty(name = "yagga.scheduler.updateRepositories.enabled", havingValue = "true")
+public class RepositoriesUpdateScheduler {
+
+    private static final Logger logger = LoggerFactory.getLogger(RepositoriesUpdateScheduler.class);
+
+    private Repositories repositories;
+
+    private StopWatch stopWatch;
+
+    @Autowired
+    public RepositoriesUpdateScheduler(Repositories repositories) {
+        this.repositories = repositories;
+        this.stopWatch = new StopWatch();
+    }
+
+    @Scheduled(fixedRateString = "${yagga.scheduler.updateRepositories.intervalInMillis}")
+    public void refreshRepositories() {
+        logger.info("Updating repositories...");
+        stopWatch.start();
+
+        repositories.getRepositories().forEach(DvcsRepository::pull);
+
+        stopWatch.stop();
+        logger.info("Updated {} repositories in {} seconds", repositories.getRepositories().size(),
+                stopWatch.getLastTaskTimeMillis() / 1000.0);
+    }
+}
